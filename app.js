b29e5444d21f5de62a37f78e88ffdb8309e5a184
codewithrishabhh~ <rishabhsahu6196@gmail.com>
2024-01-15 16:42:11 +0530

    updatedCount

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ let btnIncrement = document.querySelector("#increment")
 let btnDecrement = document.querySelector("#decrement")
 let reset = document.querySelector("#reset")
 btnIncrement.addEventListener("click", function(){
-    if (curVal >= 20){
-        alert("cannot go below")
-    }
+  
     curVal++
     mainTitle.textContent = curVal
 })
 btnDecrement.addEventListener("click", function(){
-    if (curVal <=0){
-        alert("cannot go below")
-    }
+   
     curVal--
     mainTitle.textContent = curVal
 })
